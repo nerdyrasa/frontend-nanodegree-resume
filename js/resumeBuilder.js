1becf115b67beb450e6c30edd5905564ce36c7f9
nerdyrasa <nerdyrasa@users.noreply.github.com>
2017-01-28 15:39:51 -0600

    Update README.  Rename original READM to project-rubric.md

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,29 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+
+var formattedName = HTMLheaderName.replace("%data%", "RB Isaacson");
+
+var formattedRole = HTMLheaderRole.replace("%data%", "Developer");
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+
+var skills = ["awesomeness", "programming", "teaching", "JS"];
+
+$("#main").append(skills.length);
+
+var bio = {
+  "name" : "James",
+  "role" : "Developer",
+  "contacts" : {
+    "mobile" : "x",
+    "email" : "x",
+    "github" : "x",
+    "twitter" : "x",
+    "location" : "x"
+  },
+  "welcomeMessage" : "blah blah blah",
+  "skills" : skills,
+  "bioPic" : "images/fry.jpg"
+};
+
+$("#main").append(bio.name);
