615f588e682d3d0f4919b1fed88427cb6eac55d6
nerdyrasa <nerdyrasa@users.noreply.github.com>
2017-01-30 16:02:35 -0600

    Created bio object literal and used it to dynamically generate the header div.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,29 +1,58 @@
-
-
-var formattedName = HTMLheaderName.replace("%data%", "RB Isaacson");
-
-var formattedRole = HTMLheaderRole.replace("%data%", "Developer");
-
-$("#header").append(formattedName);
-$("#header").append(formattedRole);
-
-var skills = ["awesomeness", "programming", "teaching", "JS"];
-
-$("#main").append(skills.length);
-
 var bio = {
-  "name" : "James",
+  "name" : "Jane Doette",
   "role" : "Developer",
   "contacts" : {
-    "mobile" : "x",
-    "email" : "x",
-    "github" : "x",
-    "twitter" : "x",
-    "location" : "x"
+    "mobile" : "123-456-7890",
+    "email" : "jane@xxx-janedoette.com",
+    "github" : "xxx-janedoette",
+    "twitter" : "@xxx-some-jane-doette",
+    "location" : "everywhere"
   },
-  "welcomeMessage" : "blah blah blah",
-  "skills" : skills,
+  "welcomeMessage" : "Problem solving is fun!",
+  "skills" : ["JavaScript", "HTML5", "CSS3", "C#", ".NET", "Entity Framework"],
   "bioPic" : "images/fry.jpg"
 };
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+var formattedPic = HTMLbioPic.replace("%data%", bio.bioPic);
+//var formattedContacts = HTMLcontactGeneric.replace("%data%", bio.role);
+var formattedMobile =  HTMLmobile.replace("%data%", bio.contacts.mobile);
+var formattedEmail =  HTMLemail.replace("%data%", bio.contacts.email);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+var formattedGithub =  HTMLgithub.replace("%data%", bio.contacts.github);
+//var formattedBlog = HTMLblog.replace("%data%, = bio.contacts.'
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+
+
+//$("#header").append(formattedPic);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#topContacts").append(formattedMobile);
+$("#topContacts").append(formattedEmail);
+$("#topContacts").append(formattedTwitter);
+$("#topContacts").append(formattedGithub);
+$("#topContacts").append(formattedLocation);
+$("#header").append(formattedPic);
+
+var formattedMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+$("#header").append(formattedMessage);
+
+$("#header").append(HTMLskillsStart);
+
+for ( var i=0 ; i< bio.skills.length; i++ ) {
+
+  var formattedSkills = HTMLskills.replace("%data%", bio.skills[i]);
+
+  $("#skills").append(formattedSkills);
+}
+
+
+
+
+
+
+
 
 $("#main").append(bio.name);
